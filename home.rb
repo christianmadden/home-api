3c5486e1754851795c0fb87d2db578a3fddf4c1f
Christian Madden <me@christianmadden.com>
2016-12-06 20:58:49 -0500

    Only downstairs bulbs... for now

--- home.rb
+++ home.rb
@@ -21,58 +21,39 @@ class Home
     @smartthings.exec(routine)
     case routine
     when 'morning', 'daytime', 'bedtime',
-      @hue.turn_light_off("Porch")
-      @hue.turn_light_off("Entryway")
       @hue.turn_light_off("Living Room Floor Lamp")
       @hue.turn_light_off("Living Room Table Lamp")
       @hue.turn_light_off("Cube Lamp")
-      @hue.turn_light_off("Hallway")
-      @hue.turn_light_off("Study")
-      @hue.turn_light_off("Bedroom Lamp, Christian")
-      @hue.turn_light_off("Rope Light")
       #@nest.home!
     when 'morning-away', 'daytime-away'
-      @hue.turn_light_off("Porch")
-      @hue.turn_light_off("Entryway")
       @hue.turn_light_off("Living Room Floor Lamp")
       @hue.turn_light_off("Living Room Table Lamp")
       @hue.turn_light_off("Cube Lamp")
-      @hue.turn_light_off("Hallway")
-      @hue.turn_light_off("Study")
-      @hue.turn_light_off("Bedroom Lamp, Christian")
-      @hue.turn_light_off("Rope Light")
       #@nest.away!
-    when 'night-away', 'late-night-away'
-      @hue.turn_light_off("Porch")
-      @hue.turn_light_off("Entryway")
-      @hue.turn_light_on("Living Room Floor Lamp", 128, 2500)
+    when 'night-away'
+      @hue.turn_light_on("Living Room Floor Lamp", 128, 2200)
       @hue.turn_light_off("Living Room Table Lamp")
-      @hue.turn_light_on("Cube Lamp", 128, 2500)
-      @hue.turn_light_on("Hallway", 64, 2300)
-      @hue.turn_light_off("Study")
-      @hue.turn_light_off("Bedroom Lamp, Christian")
-      @hue.turn_light_off("Rope Light")
+      @hue.turn_light_on("Cube Lamp", 128, 2200)
+      #@nest.away!
+    when 'late-night-away'
+      @hue.turn_light_on("Living Room Floor Lamp", 64, 2100)
+      @hue.turn_light_off("Living Room Table Lamp")
+      @hue.turn_light_on("Cube Lamp", 64, 2100)
       #@nest.away!
     when 'night'
-      @hue.turn_light_on("Entryway", 216, 2500)
       @hue.turn_light_on("Living Room Floor Lamp", 254, 2700)
       @hue.turn_light_on("Living Room Table Lamp", 254, 2700)
       @hue.turn_light_on("Cube Lamp", 254, 2700)
-      @hue.turn_light_on("Hallway", 216, 2500)
       #@nest.home!
     when 'late-night'
-      @hue.turn_light_on("Entryway", 128, 2400)
-      @hue.turn_light_on("Living Room Floor Lamp", 216, 2500)
-      @hue.turn_light_on("Living Room Table Lamp", 216, 2500)
-      @hue.turn_light_on("Cube Lamp", 216, 2500)
-      @hue.turn_light_off("Hallway")
+      @hue.turn_light_on("Living Room Floor Lamp", 128, 2200)
+      @hue.turn_light_on("Living Room Table Lamp", 128, 2200)
+      @hue.turn_light_on("Cube Lamp", 128, 2200)
       #@nest.home!
     when 'sleepy'
-      @hue.turn_light_off("Entryway")
-      @hue.turn_light_on("Living Room Floor Lamp", 64, 2200)
+      @hue.turn_light_on("Living Room Floor Lamp", 38, 2100)
       @hue.turn_light_off("Living Room Table Lamp")
-      @hue.turn_light_on("Cube Lamp", 64, 2200)
-      @hue.turn_light_off("Hallway")
+      @hue.turn_light_on("Cube Lamp", 38, 2100)
       #@nest.home!
     when 'reading'
       @hue.turn_light_on("Living Room Floor Lamp", 254, 4500)
